9af86a7184f53d1420ec1b6152edb1479465d718
denjay <295077487@qq.com>
2018-11-26 11:56:28 +0800

    改404页面

--- src/views/404.vue
+++ src/views/404.vue
@@ -9,12 +9,8 @@
       </div>
       <div class="bullshit">
         <div class="bullshit__oops">OOPS!</div>
-        <div class="bullshit__info">版权所有
-          <a class="link-type" href="https://wallstreetcn.com" target="_blank">华尔街见闻</a>
-        </div>
-        <div class="bullshit__headline">{{ message }}</div>
-        <div class="bullshit__info">请检查您输入的网址是否正确，请点击以下按钮返回主页或者发送错误报告</div>
-        <a href="" class="bullshit__return-home">返回首页</a>
+        <div class="bullshit__headline">THE PAGE YOU WERE LOOKING FOR DOESN'T EXIST!</div>
+        <a href="" class="bullshit__return-home">HOME PAGE</a>
       </div>
     </div>
   </div>
@@ -23,12 +19,7 @@
 <script>
 
 export default {
-  name: 'Page404',
-  computed: {
-    message() {
-      return '网管说这个页面你不能进......'
-    }
-  }
+  name: 'Page404'
 }
 </script>
 
@@ -158,7 +149,7 @@ export default {
   .bullshit {
     position: relative;
     float: left;
-    width: 300px;
+    width: 350px;
     padding: 30px 0;
     overflow: hidden;
     &__oops {
